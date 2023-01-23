fb2be10459b813711043fcbf9535b4758d8cf6cc
codeit <yhm6750@gmail.com>
2023-01-23 18:28:57 +0900

    Revert "Add squre function"
    This reverts commit 15b9637e4c0066336d88990872d23e7405168de4.

--- calculator.py
+++ calculator.py
@@ -1,6 +1,7 @@
 # 기본 계산기
 def add(a, b):
   return a+b
+
   
 def subtract(a, b):
   return a-b
@@ -10,6 +11,3 @@ def multiply(a,b);
 
 def divide_free(a, b):  
     return a/b
-
-def square(a):
-    return a*a
