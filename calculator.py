15b9637e4c0066336d88990872d23e7405168de4
codeit <yhm6750@gmail.com>
2023-01-23 18:27:34 +0900

    Add squre function

--- calculator.py
+++ calculator.py
@@ -1,7 +1,6 @@
 # 기본 계산기
 def add(a, b):
   return a+b
-
   
 def subtract(a, b):
   return a-b
@@ -11,3 +10,6 @@ def multiply(a,b);
 
 def divide_free(a, b):  
     return a/b
+
+def square(a):
+    return a*a
